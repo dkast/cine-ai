ff260052c12c21aded5ac433a847c5132859040b
Daniel Castillejo <devcastillejo@gmail.com>
2023-03-03 01:03:20 -0600

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "~/styles/globals.css";
+
+const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "WatchGPT",
+    template: "%s | WatchGPT",
+  },
+  description: "Obten recomendaciones de cine y TV con AI",
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    title: "WatchGPT",
+    type: "website",
+    url: "https://watchgpt.vercel.app",
+    // images: [
+    //   {
+    //     url: "https://dkast.dev/og.jpg",
+    //     width: 1200,
+    //     height: 675
+    //   }
+    // ]
+  },
+  //   twitter: {
+  //     title: "Daniel Castillejo",
+  //     card: "summary_large_image",
+  //     description: "Sitio personal de Daniel Castillejo",
+  //     creator: "@dkast",
+  //     images: ["https://dkast.dev/og.jpg"]
+  //   }
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <head />
+      <body className="min-h-screen">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-5xl">
+      <div>
+        <h1 className="text-3xl font-bold">
+          Recomendaciones de cine y TV impulsadas por inteligencia artificial
+        </h1>
+      </div>
+    </main>
+  );
+}
