a8cbfe11be9caf7d75c8ec9ad2a974ffb44d9bc1
Daniel Castillejo <devcastillejo@gmail.com>
2023-03-31 17:38:36 -0500

    Change runtime to edge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@ import { Clapperboard } from "lucide-react"
 import Link from "next/link"
 
 import GenerateResults from "@/components/generate-results"
+import GitHubLogo from "@/components/github-logo"
 import SearchMedia from "@/components/search-media"
 import SelectKind from "@/components/select-kind"
-import GitHubLogo from "@/components/github-logo"
 
 export default function Home() {
   return (
     <div className="mx-auto flex max-w-3xl flex-col">
-      <header className="py-8 flex justify-between px-3 sm:px-1">
+      <header className="flex justify-between px-3 py-8 sm:px-1">
         <div className="flex items-center gap-2">
           <Clapperboard className="h-8 w-8 text-amber-400" />
           <span className="text-2xl text-white">CineAI</span>
         </div>
-        <Link href="https://github.com/dkast/watch-gpt" className="border hover:bg-gray-800 border-gray-400 rounded-full flex py-2 px-3 gap-2">
+        <Link
+          href="https://github.com/dkast/watch-gpt"
+          className="flex gap-2 rounded-full border border-gray-400 px-3 py-2 hover:bg-gray-800"
+        >
           <GitHubLogo />
           <span>Ver en GitHub</span>
         </Link>
       </header>
-      <main className="flex flex-col gap-10 py-16 px-3 text-center sm:py-24 sm:px-1">
+      <main className="flex flex-col gap-10 px-3 py-16 text-center sm:px-1 sm:py-24">
         <h1 className="text-3xl font-bold text-gray-50 sm:text-4xl">
           <Balancer>
             Descubre tu próxima serie favorita o película con la ayuda de IA.
@@ -34,8 +37,23 @@ export default function Home() {
         <SelectKind />
         <GenerateResults />
       </main>
-      <footer className="text-center py-8 text-sm">
-        <Balancer>Desarrollado por <Link href="https://dkast.dev" className="text-gray-200 hover:underline">Daniel Castillejo</Link>, habilitado por <Link href="https://openai.com/blog/chatgpt" className="text-gray-200 hover:underline">ChatGPT</Link>.
+      <footer className="py-8 text-center text-sm">
+        <Balancer>
+          Desarrollado por{" "}
+          <Link
+            href="https://dkast.dev"
+            className="text-gray-200 hover:underline"
+          >
+            Daniel Castillejo
+          </Link>
+          , habilitado por{" "}
+          <Link
+            href="https://openai.com/blog/chatgpt"
+            className="text-gray-200 hover:underline"
+          >
+            ChatGPT
+          </Link>
+          .
         </Balancer>
       </footer>
     </div>
